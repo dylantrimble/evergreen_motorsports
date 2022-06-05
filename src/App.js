1c836cee7def08185a98fe19d39f44cc99f436ea
Dylan Trimble <dyltrimble12@gmail.com>
2022-06-05 13:19:38 -0400

    Fixed a few css issues with product cards and added new mobile products page banner

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
     }
   }
   return (
-    <div className="App">
+    <>
       <NavBar />
       <HeroCarousel />
 
@@ -38,7 +38,7 @@ function App() {
           ))}
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
