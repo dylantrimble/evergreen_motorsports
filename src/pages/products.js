5cbf36970a130520a032280e0758dd376a15c521
Dylan Trimble <dyltrimble12@gmail.com>
2022-06-12 12:45:21 -0400

    added filter buttons with style for all screen sizes. No functionality yet

--- src/pages/products.js
+++ src/pages/products.js
@@ -10,22 +10,44 @@ function products() {
   console.log(items);
   return (
     <>
-      <NavBar/>
-      <img className="prod-hero-dt" src="/images/home-imgs/EGM-Long-Logo-Slap-HKS.png" alt='EGM logo'/>
+      <NavBar />
+      <img className="prod-hero-dt" src="/images/home-imgs/EGM-Long-Logo-Slap-HKS.png" alt='EGM logo' />
       <div className="prod-hero-mobile"></div>
-      <div className="prod-container">
-        {items.map(item => (
-          <ProductCard
-          title={item.title}
-          image = {item.image}
-          id = {item.id}
-          price = {item.price}
-          category = {item.category}
-          desc = {item.desc}
-          />
-        ))}
+      <div className="prod-page-grid">
+        <aside className='filter-container'>
+            <div className='filter-options'>
+              <label htmlFor="type">Filter by type:</label>
+              <select name="type" id="fbt">
+                <option value="wheels">Wheels</option>
+                <option value="seats">Seats</option>
+                <option value="suspension">Suspension</option>
+                <option value="stickers">Stickers</option>
+              </select>
+            </div>
+            <div className='sort-options'>
+              <label htmlFor="type">Sort:</label>
+              <select name="type" id="fbt">
+                <option value="ascending">Price (high to low)</option>
+                <option value="decending">Price (low to high)</option>
+                <option value="alphabetical">Alphabetical</option>
+                <option value="type">Type</option>
+              </select>
+            </div>
+        </aside>
+        <div className="prod-container">
+          {items.map(item => (
+            <ProductCard
+              title={item.title}
+              image={item.image}
+              id={item.id}
+              price={item.price}
+              category={item.category}
+              desc={item.desc}
+            />
+          ))}
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   )
 }
